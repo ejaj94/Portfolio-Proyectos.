830917d78ac52880e00f8933d93ce921b9602882
EnmanuelJimenez <ejaj94@users.noreply.github.com>
2026-09-01 10:39:31 +0100

    fix: replace Chicken Crunch Crispy Burger image with exact user provided photograph

--- Proyecto 18 - Fast Food Burger and Crunch/app.py
+++ Proyecto 18 - Fast Food Burger and Crunch/app.py
@@ -96,5 +96,5 @@ def list_orders():
 
 
 if __name__ == "__main__":
-    print("Iniciando BURGER & CRUNCH Fast Food Web App (Vilamoura) em http://localhost:5041")
-    app.run(host="0.0.0.0", port=5041, debug=False)
+    print("Iniciando BURGER & CRUNCH Fast Food Web App (Vilamoura) em http://localhost:5042")
+    app.run(host="0.0.0.0", port=5042, debug=False)
