db66474d0386fd1a74dd9606df2a6c7e2b3131cb
EnmanuelJimenez <ejaj94@users.noreply.github.com>
2026-09-01 10:35:14 +0100

    fix: replace individual Smash Burger Duplo Bacon Cheddar image with exact user provided photograph

--- Proyecto 18 - Fast Food Burger and Crunch/app.py
+++ Proyecto 18 - Fast Food Burger and Crunch/app.py
@@ -96,5 +96,5 @@ def list_orders():
 
 
 if __name__ == "__main__":
-    print("Iniciando BURGER & CRUNCH Fast Food Web App (Vilamoura) em http://localhost:5038")
-    app.run(host="0.0.0.0", port=5038, debug=False)
+    print("Iniciando BURGER & CRUNCH Fast Food Web App (Vilamoura) em http://localhost:5039")
+    app.run(host="0.0.0.0", port=5039, debug=False)
