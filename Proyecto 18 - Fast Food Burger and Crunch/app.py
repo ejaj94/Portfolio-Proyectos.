ff389612b5bf57445cb433c024fa43657122a37d
EnmanuelJimenez <ejaj94@users.noreply.github.com>
2026-09-01 10:22:43 +0100

    fix: replace Combo Pizza Pepperoni & Wings image with exact user provided photograph

--- Proyecto 18 - Fast Food Burger and Crunch/app.py
+++ Proyecto 18 - Fast Food Burger and Crunch/app.py
@@ -96,5 +96,5 @@ def list_orders():
 
 
 if __name__ == "__main__":
-    print("Iniciando BURGER & CRUNCH Fast Food Web App (Vilamoura) em http://localhost:5035")
-    app.run(host="0.0.0.0", port=5035, debug=False)
+    print("Iniciando BURGER & CRUNCH Fast Food Web App (Vilamoura) em http://localhost:5036")
+    app.run(host="0.0.0.0", port=5036, debug=False)
