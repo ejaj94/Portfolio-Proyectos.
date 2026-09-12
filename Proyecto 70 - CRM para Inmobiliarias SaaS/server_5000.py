041789b190cba8a8df8947c7e3040e0d02391338
EnmanuelJimenez <ejaj94@users.noreply.github.com>
2026-09-12 17:31:36 +0100

    feat: add additional showcase servers on ports 9090 and 5000

---
+++ Proyecto 70 - CRM para Inmobiliarias SaaS/server_5000.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route('/')
+@app.route('/showcase')
+def showcase():
+    return render_template('showcase.html')
+
+if __name__ == '__main__':
+    print("[Starting Showcase Server] Host: http://127.0.0.1:5000 ...")
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
